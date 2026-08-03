51caaa656771c31875a55948119f4827532ec393
Anibeeran <anibeeran22@gmail.com>
2026-08-03 17:07:29 +0530

    Fixed CORS for vercel

--- src/main/java/com/shopkart/config/CorsConfig.java
+++ src/main/java/com/shopkart/config/CorsConfig.java
@@ -17,8 +17,9 @@ public class CorsConfig {
 			public void addCorsMappings(CorsRegistry registry) {
 				
 				registry.addMapping("/**")
-				.allowedOrigins("http://localhost:5173")
-				.allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
+				.allowedOrigins("http://localhost:5173",
+						"https://shopkart-frontend-yd6e.vercel.app")
+				.allowedMethods("*")
 				.allowedHeaders("*");
 			}
 		};
